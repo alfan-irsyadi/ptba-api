91e158ff949bf79af80081910bc78342f3a8017f
alfan-irsyadi <irsyadialfan@gmail.com>
2023-02-05 22:42:58 +0700

    edit index

--- index.js
+++ index.js
@@ -1,38 +1,28 @@
 const app = require('express')();
 let chrome = {};
 let puppeteer;
+const chromium = require('chrome-aws-lambda')
 
 app.get('/api', async (req, res) => {
-    chrome = require('chrome-aws-lambda');
-    if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
-        // running on the Vercel platform.        
-        puppeteer = require('puppeteer-core');
-    } else {
-        // running locally.        
-        puppeteer = require('puppeteer');
-    }
+    // import * as puppeteer from 'puppeteer';
 
-    try {
-        let browser = await puppeteer.launch({            
-            args: chrome.arg,
-            defaultViewport: chrome.defaultViewport,
-            executablePath: await chrome.executablePath,
-            headless: true,
-            ignoreHTTPSErrors: true,
-        });
-        const page = await browser.newPage()
-        await page.goto('https://api.investing.com/api/financialdata/101599/historical/chart/?period=P5Y&interval=P1W&pointscount=120')        
-        console.log(page)
-        await page.waitForSelector('pre')
-        let pre = await page.$('pre')
-        let content = await page.evaluate(el => el.textContent, pre)
-        res.send(content)
-        console.log(content)
-        await browser.close()
-    } catch (err) {
-        console.error(err);
-        return null;
-    }
+    // const browser = await puppeteer.launch( { args: ['--no-sandbox'] } );
+
+    const browser = await chromium.puppeteer.launch({
+        args: [...chromium.args, "--hide-scrollbars", "--disable-web-security"],
+        defaultViewport: chromium.defaultViewport,
+        executablePath: await chromium.executablePath,
+        headless: false,
+        ignoreHTTPSErrors: true,
+    })
+    const page = await browser.newPage()
+    await page.goto('https://api.investing.com/api/financialdata/101599/historical/chart/?period=P5Y&interval=P1W&pointscount=120')
+    // await page.waitForSelector('pre')
+    let pre = await page.$('pre')
+    let content = await page.evaluate(el => el.textContent, pre)
+    console.log(content)    
+    await browser.close()
+    res.send(JSON.parse(content))
 })
 app.listen(process.env.PORT || 3000, () => {
     console.log('started')
