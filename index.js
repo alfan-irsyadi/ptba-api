fdd9d7a6e29703af698f451d6922d84593afe7fb
alfan-irsyadi <irsyadialfan@gmail.com>
2023-02-05 20:23:28 +0700

    edit

--- index.js
+++ index.js
@@ -1,27 +1,42 @@
-const chromium = require('chrome-aws-lambda')
-const {addExtra} = require('puppeteer-extra')
-const puppeteerExtra = addExtra(chromium.puppeteer)
 const app = require('express')();
+let chrome = {};
+let puppeteer;
 
 app.get('/api', async (req, res) => {
-    let browser = await puppeteerExtra
-        .launch({
-            args: chromium.args,
-            defaultViewport: chromium.defaultViewport,
-            executablePath: await chromium.executablePath,
+    chrome = require('chrome-aws-lambda');
+    if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+        // running on the Vercel platform.
+        
+        puppeteer = require('puppeteer-core');
+    } else {
+        // running locally.        
+        puppeteer = require('puppeteer');
+    }
+
+    try {
+        let browser = await puppeteer.launch({
+            args: [...chrome.args, '--hide-scrollbars', '--disable-web-security'],
+            defaultViewport: chrome.defaultViewport,
+            executablePath: await chrome.executablePath,
             headless: true,
-            ignoreHTTPSErrors: true
-        })
-    const page = await browser.newPage()
-    await page.goto('https://api.investing.com/api/financialdata/101599/historical/chart/?period=P5Y&interval=P1W&pointscount=120')
-    let pre = await page.$('pre')
-    let content = await page.evaluate(el => el.textContent, pre)
-    res.send(content)
-    console.log(content)
-    await browser.close()
-
-
+            ignoreHTTPSErrors: true,
+        });
+        const page = await browser.newPage()
+        await page.goto('https://api.investing.com/api/financialdata/101599/historical/chart/?period=P5Y&interval=P1W&pointscount=120')
+        let pre = await page.$('pre')
+        let content = await page.evaluate(el => el.textContent, pre)
+        res.send(content)
+        console.log(content)
+        await browser.close()
+    } catch (err) {
+        console.error(err);
+        return null;
+    }
 })
-app.listen(process.env.PORT||3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log('started')
 })
+
+
+
+
